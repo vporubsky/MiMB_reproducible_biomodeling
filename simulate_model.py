f972dd7f9762b4406d632954ec059e43da7176f4
Veronica Porubsky <verosky@uw.edu>
2021-02-17 15:44:49 -0800

    Added program to simulate repressilator model.

---
+++ simulate_model.py
@@ -0,0 +1,15 @@
+'''Author: Veronica Porubsky
+Description: Script to simulate BIOMD0000000012 (Elowitz repressilator model, 2000)
+
+See: https://www.ebi.ac.uk/biomodels/BIOMD0000000012 for model documentation on BioModels Database.'''
+import tellurium as te
+import tkinter
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+repressilator_mod = te.loadSBMLModel("https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000012?filename=BIOMD0000000012_url.xml")
+simulation_result = repressilator_mod.simulate(0, 500, 1000)
+repressilator_mod.plot(figsize = (10, 6), xtitle = 'Time', ytitle = 'Concentration')
+plt.show()
+
+#%% write and export SED-ML
