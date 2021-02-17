99a37738b204b4a387428e73727df90ebb1a29d5
Veronica Porubsky <verosky@uw.edu>
2021-02-17 15:43:24 -0800

    Added program to import repressilator model from BioModels Database.

--- import_model.py
+++ import_model.py
@@ -1,6 +1,6 @@
 '''
 Author: Veronica Porubsky
-Description: Script to import BIOMD0000000012 (Elowitz repressilator model, 20
+Description: Script to import BIOMD0000000012 (Elowitz repressilator model, 2000)
 
 See: https://www.ebi.ac.uk/biomodels/BIOMD0000000012 for model documentation on BioModels Database.'''
 import tellurium as te
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 repressilator_mod = te.loadSBMLModel("https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000012?filename=BIOMD0000000012_url.xml")
-repressilator_mod.simulate(0, 500, 1000)
+simulation_result = repressilator_mod.simulate(0, 500, 1000)
 repressilator_mod.plot(figsize = (10, 6), xtitle = 'Time', ytitle = 'Concentration')
 plt.show()
-
-#%% import
